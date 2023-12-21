9a72d3dcad13198684123f28786844c431f6f1c8
rawendra <rawendra@gmail.com>
2023-12-21 16:59:54 +0530

    adding sign-in sign-out button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-import { Button } from "@nextui-org/react"
-
+import { Button } from "@nextui-org/react";
+import { signIn , signOut} from "@/actions";
 export default function Home() {
-  return ( 
-    <div><Button>CLICK ME</Button></div>
-  )
+  return (
+    <div>
+      <form action={signIn}>
+        <Button type="submit">SIGN IN</Button>
+      </form>
+      <form action={signOut}>
+        <Button type="submit">SIGN OUT</Button>
+      </form>
+    </div>
+  );
 }
